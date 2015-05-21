513e507185d71fd6b7f988b09e7eafd65f74a1ff
Silence <yb_17136@163.com>
2015-05-21 23:15:14 +0800

    commit gitignore and dataGater

--- src/com/yangbang/beatboxteach/util/DataFactory.java
+++ src/com/yangbang/beatboxteach/util/DataFactory.java
@@ -4,10 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DataFactory {
+	//文字教学系列
 	public String dataWeb1="http://zhidao.baidu.com/link?url=hMJzPNDslglS_h2r26b4IJ1Yl8h6VAyRUCoNQ2lbtq0hnpiyRMfW54QhGttYD-9oAXinRqPAojdsaSHT7n3W2q";
 	public String dataWeb2="http://wenku.baidu.com/link?url=A8P2ZKEz01tG_zpSoZOXZg_oL8QTf9Iyhifs-tqq3DdisX_ZimOtoJzfZ03vcOpSMeweH8kMeNtxjeRkQrmeUzVouM2Z_GLPYNErryQRR_G";
 	public String dataWeb3="http://tieba.baidu.com/p/2979373855";
-	public String dataWeb4="";
+	//double教学系列
+	public String dataWeb4="http://www.youku.com/playlist_show/id_22597454_ascending_1_mode_detail_page_1.html";
+	//Krnfx教学系列
+	public String dataWeb5="http://www.youku.com/playlist_show/id_23213969.html?sf=10100";
+	
 	public static Map<String, Map<String, String>> mapVoice = new HashMap<String, Map<String, String>>();
 	public static String text_title_str[] = { "基础音篇", "特殊音篇", "Beat篇" };
 

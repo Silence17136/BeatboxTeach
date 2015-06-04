470335da3c28e88388b5a5d1e07eccc20a6baa6e
Silence17136 <yb_17136@163.com>
2015-06-04 18:30:47 +0800

    caompany commit optomize webview

--- src/com/yangbang/beatboxteach/TeachActivity.java
+++ src/com/yangbang/beatboxteach/TeachActivity.java
@@ -2,11 +2,11 @@ package com.yangbang.beatboxteach;
 
 import net.youmi.android.spot.SpotManager;
 import android.annotation.SuppressLint;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ScrollView;
@@ -54,6 +54,7 @@ public class TeachActivity extends BaseActivity {
 		initData();
 	}
 
+	@SuppressWarnings("deprecation")
 	@SuppressLint("SetJavaScriptEnabled")
 	private void initData() {
 		Teaching teaching = null;
@@ -78,6 +79,20 @@ public class TeachActivity extends BaseActivity {
 				activity_teach_scroll.setVisibility(View.GONE);
 				activity_teach_webview.loadUrl(teaching.getContent());
 				activity_teach_webview.getSettings().setJavaScriptEnabled(true);
+				activity_teach_webview.getSettings().setPluginState(
+						PluginState.ON);
+				activity_teach_webview.getSettings()
+						.setJavaScriptCanOpenWindowsAutomatically(true);
+				activity_teach_webview.getSettings().setAllowFileAccess(true);
+				activity_teach_webview.getSettings()
+						.setDefaultTextEncodingName("UTF-8");
+				activity_teach_webview.getSettings().setLoadWithOverviewMode(
+						true);
+				// activity_teach_webview.getSettings().setBuiltInZoomControls(true);
+				activity_teach_webview.getSettings().setUseWideViewPort(true);
+				activity_teach_webview.getSettings().setDomStorageEnabled(true);
+				// 应用可以有缓存
+				// activity_teach_webview.getSettings().setAppCacheEnabled(true);
 				activity_teach_webview.setWebViewClient(new WebViewClient() {
 					@Override
 					public boolean shouldOverrideUrlLoading(WebView view,
@@ -102,31 +117,29 @@ public class TeachActivity extends BaseActivity {
 			}
 		}
 	}
-	
+
 	public void onBackPressed() {
 
-	    // 如果有需要，可以点击后退关闭插播广告。
-	    if (!SpotManager.getInstance(this).disMiss()) {
-	        // 弹出退出窗口，可以使用自定义退屏弹出和回退动画,参照demo,若不使用动画，传入-1
-	        super.onBackPressed();
-	    }
+		// 如果有需要，可以点击后退关闭插播广告。
+		if (!SpotManager.getInstance(this).disMiss()) {
+			// 弹出退出窗口，可以使用自定义退屏弹出和回退动画,参照demo,若不使用动画，传入-1
+			super.onBackPressed();
+		}
 	}
 
 	@Override
-
 	protected void onStop() {
 
-	    // 如果不调用此方法，则按home键的时候会出现图标无法显示的情况。
-	    SpotManager.getInstance(this).onStop();
-	    super.onStop();
+		// 如果不调用此方法，则按home键的时候会出现图标无法显示的情况。
+		SpotManager.getInstance(this).onStop();
+		super.onStop();
 	}
 
 	@Override
-
 	protected void onDestroy() {
 
-	    SpotManager.getInstance(this).onDestroy();
-	    super.onDestroy();
+		SpotManager.getInstance(this).onDestroy();
+		super.onDestroy();
 	}
 
 	// @Override
